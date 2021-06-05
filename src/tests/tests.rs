5edf5a1e4fa2d42fb7f4d6b78025c1a30b4918ff
William Venner <william@venner.io>
2021-06-05 23:34:59 +0100

    Fix tests for 32-bit older LuaJIT bytecode

--- src/tests/tests.rs
+++ src/tests/tests.rs
@@ -1,4 +1,13 @@
 #[cfg(test)]
+fn verify_hello_world_bytecode(bytecode: &[u8]) {
+	#[cfg(target_pointer_width = "64")]
+	assert_eq!(bytecode, [27, 76, 74, 2, 10, 43, 2, 0, 3, 0, 2, 0, 4, 54, 0, 0, 0, 39, 2, 1, 0, 66, 0, 2, 1, 75, 0, 1, 0, 18, 72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100, 33, 10, 112, 114, 105, 110, 116, 0]);
+
+	#[cfg(target_pointer_width = "32")]
+	assert_eq!(bytecode, [27, 76, 74, 1, 2, 43, 2, 0, 2, 0, 2, 0, 4, 52, 0, 0, 0, 37, 1, 1, 0, 62, 0, 2, 1, 71, 0, 1, 0, 18, 72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100, 33, 10, 112, 114, 105, 110, 116, 0]);
+}
+
+#[cfg(test)]
 fn check_stack(compiler: crate::compiler::BytecodeCompiler) {
 	assert_eq!(compiler.stack_size(), 3);
 	assert_eq!(compiler.get_type(compiler.stack_size()), "function");
@@ -7,7 +16,7 @@ fn check_stack(compiler: crate::compiler::BytecodeCompiler) {
 #[cfg(test)]
 fn compile_hello_world_string(compiler: &crate::compiler::BytecodeCompiler) {
 	let bytecode = compiler.compile_string(lua_string!(r#"print("Hello, world!")"#), true).unwrap();
-	assert_eq!(bytecode, [27, 76, 74, 2, 10, 43, 2, 0, 3, 0, 2, 0, 4, 54, 0, 0, 0, 39, 2, 1, 0, 66, 0, 2, 1, 75, 0, 1, 0, 18, 72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100, 33, 10, 112, 114, 105, 110, 116, 0]);
+	verify_hello_world_bytecode(&bytecode);
 }
 
 #[cfg(test)]
@@ -31,7 +40,7 @@ fn compile_invalid_file(compiler: &crate::compiler::BytecodeCompiler) {
 #[cfg(test)]
 fn compile_hello_world_file(compiler: &crate::compiler::BytecodeCompiler) {
 	let bytecode = compiler.compile_file(lua_string!(std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("src/tests/hello_world.lua").to_string_lossy().to_string()), true).unwrap();
-	assert_eq!(bytecode, [27, 76, 74, 2, 10, 43, 2, 0, 3, 0, 2, 0, 4, 54, 0, 0, 0, 39, 2, 1, 0, 66, 0, 2, 1, 75, 0, 1, 0, 18, 72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100, 33, 10, 112, 114, 105, 110, 116, 0]);
+	verify_hello_world_bytecode(&bytecode);
 }
 
 #[test]
